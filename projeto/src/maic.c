25cf1b6476777a46fe3a508ca2c494945b44acba
DenisRdSilva <den_you@hotmail.com>
2025-09-26 12:51:03 -0300

    Atualizando as operações das matrizes

--- projeto/src/maic.c
+++ projeto/src/maic.c
@@ -1,51 +1,81 @@
 #include <stdio.h>
 
-int inserirLinhas() { // função que insere o número de linhas
-    int numero;
+int escolherOperacao() { // função que escolhe a operação a ser feita com as matrizes
+    int operacao;
 
-    printf("Insira o número de linhas da matriz: ");
-    scanf("%d", &numero);
+    do { // Só prossegue se o usuário escolher uma opção válida
+        printf("Escolha a operação:\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n");
+        scanf("%d", &operacao);
+    } while (operacao < 1 || operacao > 3);
 
-    return numero;
+    return operacao;
 }
 
-int inserirColunas() { // função que insere o número de colunas
+int inserirLinhasOuColunas(int tipo) { // função que insere o número de linhas ou colunas
     int numero;
 
-    printf("Insira o número de colunas da matriz: ");
+    if (tipo) {
+        printf("Insira o número de colunas da matriz: ");
+    } else {
+        printf("Insira o número de linhas da matriz: ");
+    }
+
     scanf("%d", &numero);
 
     return numero;
 }
 
 int main(){
-    int linhas = inserirLinhas();
-    int colunas = inserirColunas();
+    int operacao = escolherOperacao();
+    int linhas = inserirLinhasOuColunas(0);
+    int colunas = inserirLinhasOuColunas(1);
 
-    int matriz[linhas][colunas];
+    int matriz1[linhas][colunas];
+    int matriz2[linhas][colunas];
+    int resultado[linhas][colunas];
 
-    int criarMatriz(int linhas, int colunas) { // função que cria a matriz
+    void criarMatriz(int linhas, int colunas, int matriz[linhas][colunas], int indice) { // função que cria a matriz
         for (int i = 0; i < linhas; i++) {
             for (int j = 0; j < colunas; j++) {
-                printf("Insira o valor para a posição %d%d: ", i+1, j+1);
+                printf("Insira o elemento %d%d da matriz %d: ", i+1, j+1, indice);
                 scanf("%d", &matriz[i][j]);
             }
         }
     }
 
-    criarMatriz(linhas, colunas);
-
-    for (int i = 0; i < linhas; i++) {
-        for (int j = 0; j < colunas; j++) {
-            printf("%d", matriz[i][j]);
-            if (j < colunas - 1) {
-                printf(" ");
-            }
-            if (j == colunas - 1) {
-                printf("\n");
+    void imprimirMatriz(int linhas, int colunas, int matriz[linhas][colunas]) { // função que imprime a matriz
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                printf("%d", matriz[i][j]);
+                if (j < colunas - 1) {
+                    printf(" ");
+                }
+                if (j == colunas - 1) {
+                    printf("\n");
+                }
             }
         }
     }
+
+    criarMatriz(linhas, colunas, matriz1, 1);
+    criarMatriz(linhas, colunas, matriz2, 2);
+
+    void operacaoMatrizes(int linhas, int colunas) { 
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                if (operacao == 1) {
+                    resultado[i][j] = matriz1[i][j] + matriz2[i][j]; // soma os elementos das matrizes
+                } else if (operacao == 2) {
+                    resultado[i][j] = matriz1[i][j] - matriz2[i][j]; // subtrai os elementos das matrizes
+                // } else {
+                //     resultado[i][j] = matriz1[i][j] * matriz2[i][j]; // multiplica os elementos das matrizes
+                }
+            }
+        }
+        imprimirMatriz(linhas, colunas, resultado);
+    }
+
+    operacaoMatrizes(linhas, colunas);
     
     return 0;
 }   
